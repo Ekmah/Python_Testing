14dc328b83eb275db4133483f78b377c84394d30
Thomas Poupinet <th.poupinet@free.fr>
2022-11-06 14:53:28 +0100

    issue 1

--- server.py
+++ server.py
@@ -24,9 +24,15 @@ clubs = loadClubs()
 def index():
     return render_template('index.html')
 
+
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
+    try:
+        club = \
+            [club for club in clubs if club['email'] == request.form['email']][
+                0]
+    except IndexError:
+        return "Sorry, that email wasn't found."
     return render_template('welcome.html',club=club,competitions=competitions)
 
 
