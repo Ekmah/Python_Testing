70cb935b75fb76ad8e99508357b8148c6c5a0102
Thomas Poupinet <th.poupinet@free.fr>
2022-11-20 14:44:45 +0100

    general testing

--- server.py
+++ server.py
@@ -1,30 +1,58 @@
 import json
+import os
 from flask import Flask,render_template,request,redirect,flash,url_for
 
 
 def loadClubs():
-    with open('clubs.json') as c:
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, './clubs.json')
+    with open(filename) as c:
          listOfClubs = json.load(c)['clubs']
          return listOfClubs
 
 
 def loadCompetitions():
-    with open('competitions.json') as comps:
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, './competitions.json')
+    with open(filename) as comps:
          listOfCompetitions = json.load(comps)['competitions']
          return listOfCompetitions
 
 
-app = Flask(__name__)
-app.secret_key = 'something_special'
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='something_special',
+    )
 
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    return app
+
+
+app = create_app()
 competitions = loadCompetitions()
 clubs = loadClubs()
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/showSummary',methods=['POST'])
+
+@app.route('/showSummary', methods=['POST'])
 def showSummary():
     club = [club for club in clubs if club['email'] == request.form['email']][0]
     return render_template('welcome.html',club=club,competitions=competitions)
