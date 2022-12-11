5996ede6f5d3273078979842dbd667dbfb588ddc
Thomas Poupinet <th.poupinet@free.fr>
2022-12-11 17:04:16 +0100

    if form data is correct, possible places calculus, better tests

--- project_tests/test_all.py
+++ project_tests/test_all.py
@@ -6,6 +6,123 @@ from project_tests.conftest import *
 from server import book
 
 
+class TestIssue1:
+    def test_not_email(self, client):
+        email = "aaaaaaaaaa"
+        route = "/showSummary"
+        response = client.post(route, data={
+            "email": email,
+        })
+        assertion = \
+            b"Sorry, that email wasn't found."
+        assert response.data == assertion
+
+    def test_wrong_email(self, client):
+        email = "test.test@test.fr"
+        route = "/showSummary"
+        response = client.post(route, data={
+            "email": email,
+        })
+        assertion = \
+            b"Sorry, that email wasn't found."
+        assert response.data == assertion
+
+    def test_good_email(self, client):
+        email = clubs[0]['email']
+        route = "/showSummary"
+        response = client.post(route, data={
+            "email": email,
+        })
+        assert response.status_code == 200
+
+    def test_other_type(self, client):
+        email = 1
+        route = "/showSummary"
+        response = client.post(route, data={
+            "email": email,
+        })
+        assertion = \
+            b"Sorry, that email wasn't found."
+        assert response.data == assertion
+
+
+class TestIssue2:
+
+    def test_more_than_available_points(self, client):
+        route = "/purchasePlaces"
+        before_points = int(clubs[1]["points"])
+        chosen_points = int(before_points) + 1
+        resp = client.post(route, data={
+            "club": clubs[1]["name"],
+            "competition": competitions[1]["name"],
+            "places": chosen_points,
+        })
+        assertion = \
+            b'You are trying to book an impossible quantity of places.'
+        assert resp.data == assertion
+
+    def test_more_than_available_places(self, client):
+        route = "/purchasePlaces"
+        before_places = int(competitions[1]["numberOfPlaces"])
+        chosen_points = int(before_places) + 1
+        resp = client.post(route, data={
+            "club": clubs[1]["name"],
+            "competition": competitions[1]["name"],
+            "places": chosen_points,
+        })
+        assertion = \
+            b'You are trying to book an impossible quantity of places.'
+        assert resp.data == assertion
+
+    def test_correct_amount(self, client):
+        route = "/purchasePlaces"
+        before_points = int(clubs[1]["points"])
+        before_places = int(competitions[1]["numberOfPlaces"])
+        if before_points == 0:
+            chosen_points = 0
+        else:
+            chosen_points = 1
+        resp = client.post(route, data={
+            "club": clubs[1]["name"],
+            "competition": competitions[1]["name"],
+            "places": chosen_points,
+        })
+        after_places = int(competitions[1]["numberOfPlaces"])
+        after_points = int(clubs[1]["points"])
+
+        assert ((before_places - after_places) <= before_points or
+                (before_places - after_places) <= 12)
+        assert (before_places - after_places) == chosen_points
+        assert (before_points - after_points) == chosen_points
+        assert (before_places - after_places) == (before_points - after_points)
+        assert after_places >= 0
+        assert after_points >= 0
+
+    def test_null_amount(self, client):
+        route = "/purchasePlaces"
+        chosen_points = 0
+        resp = client.post(route, data={
+            "club": clubs[1]["name"],
+            "competition": competitions[1]["name"],
+            "places": chosen_points,
+        })
+        assertion = \
+            b'You are trying to book an impossible quantity of places.'
+        assert resp.data == assertion
+
+    def test_negative_amount(self, client):
+        route = "/purchasePlaces"
+        chosen_points = -1
+        resp = client.post(route, data={
+            "club": clubs[1]["name"],
+            "competition": competitions[1]["name"],
+            "places": chosen_points,
+        })
+        assertion = \
+            b'You are trying to book an impossible quantity of places.'
+        assert resp.data == assertion
+
+
 class TestIssue5:
 
     def test_future_competition_date(self, client):
@@ -21,3 +138,5 @@ class TestIssue5:
 
         resp = client.get(f"/book/{competition}/{club}")
         assert resp.data == b'Sorry, that competition has passed.'
+
+
--- server.py
+++ server.py
@@ -1,16 +1,21 @@
 import json
+import os
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, flash, url_for
 
 
 def load_clubs():
-    with open('clubs.json') as c:
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, './clubs.json')
+    with open(filename) as c:
         list_of_clubs = json.load(c)['clubs']
         return list_of_clubs
 
 
 def load_competitions():
-    with open('competitions.json') as comps:
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, './competitions.json')
+    with open(filename) as comps:
         list_of_competitions = json.load(comps)['competitions']
         return list_of_competitions
 
@@ -45,7 +50,9 @@ def book(competition, club):
     found_competition = \
         [c for c in competitions if c['name'] == competition][0]
     if found_club and found_competition:
-        possible_places = min(int(found_club['points']), 12)
+        possible_places = min(int(found_club['points']),
+                              min(12,
+                                  int(found_competition['numberOfPlaces'])))
         comp_time = datetime.strptime(found_competition["date"],
                                       "%Y-%m-%d %H:%M:%S")
         now = datetime.now()
@@ -70,6 +77,12 @@ def purchase_places():
          request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     places_required = int(request.form['places'])
+    if (
+            places_required > 12 or
+            (places_required > int(club["points"])) or
+            (places_required > int(competition['numberOfPlaces'])) or
+            places_required <= 0):
+        return "You are trying to book an impossible quantity of places."
     competition['numberOfPlaces'] = \
         int(competition['numberOfPlaces']) - places_required
     club["points"] = int(club["points"]) - places_required
